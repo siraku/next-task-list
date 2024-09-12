60d3b64a640ccf3942fea93afbac2efe790dab36
siraku <slstc6666@yahoo.co.jp>
2024-09-12 09:12:11 +0900

    add task list

--- src/app/tasks/page.js
+++ src/app/tasks/page.js
@@ -1,7 +1,22 @@
 import TaskOverview from "@/components/taskOverview";
 import React from "react";
 
-const Tasks = () => {
+const getTasks = async () => {
+  try {
+    const apiResponse = await fetch("http://localhost:3000/api/get-allTasks", {
+      method: "GET",
+      cache: "no-store",
+    });
+    const result = await apiResponse.json();
+    return result?.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+const Tasks = async () => {
+  const tasks = await getTasks();
   return (
     <div>
       <TaskOverview />
--- src/components/taskOverview/index.js
+++ src/components/taskOverview/index.js
@@ -1,12 +1,39 @@
 "use client";
 import React from "react";
 import AddTask from "../addTask";
+import { Card, CardContent, CardTitle } from "../ui/card";
 
-const TaskOverview = () => {
+const getTasks = async () => {
+  try {
+    const apiResponse = await fetch("http://localhost:3000/api/get-allTasks", {
+      method: "GET",
+      cache: "no-store",
+    });
+    const result = await apiResponse.json();
+    return result?.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+const TaskOverview = async () => {
+  const tasks = await getTasks();
+  console.log(tasks);
   return (
     <div className=" min-h-screen flex flex-col bg-gradient-to-r p-10 from-purple-500 to-blue-500">
       <div>
         <AddTask />
+      </div>
+      <div>
+        {tasks.length > 0
+          ? tasks.map((task) => (
+              <Card>
+                <CardTitle>aa</CardTitle>
+                <CardContent>bb</CardContent>
+              </Card>
+            ))
+          : null}
       </div>
     </div>
   );
