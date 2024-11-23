4aee715c9518ba0d23a2c5e602f0621f03943121
siraku <slstc6666@yahoo.co.jp>
2024-11-23 22:20:28 +0900

    modify home page

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,11 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 to-blue-500">
       <div>
-        <h2 className="text-4xl font-bold text-white mb-5">Task List</h2>
         <Link
-          className="bg-white text-sm text-blue-700 font-semibold py-2 px-6 rounded"
+          className="bg-white text-3xl text-blue-700 font-semibold py-2 px-6 rounded"
           href="/tasks"
         >
-          Explore Task
+          Go To Task List
         </Link>
       </div>
     </div>
